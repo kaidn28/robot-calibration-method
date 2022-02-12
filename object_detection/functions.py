33041c024b12d8ac7196abf0cb52b00b567d4d6d
itachi176 <lhoang17062000@gmail.com>
2022-02-12 17:39:36 +0700

    add object detector predict function

--- object_detection/functions.py
+++ object_detection/functions.py
@@ -1,1 +1,11 @@
 #support functions for object detection
+import torch 
+
+def yolo(image):
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='./model/super-best.pt')
+    model.conf = 0.6
+    model.iou = 0.6
+    img = image[..., ::-1]
+    results = model(img)
+    a = results.pandas().xywh[0]
+    return a 
--- tools/predict.py
+++ tools/predict.py
@@ -11,23 +11,23 @@ from calibration import Calibrator
 from object_detection import ObjectDetector
 def parse_args():
     parser =argparse.ArgumentParser(description="Testing pipeline")
-    parser.add_argument('--image', type=str, help='path to image', default= './datasets/object_images/dataset_29072021/img1.jpg')
+    parser.add_argument('--image', type=str, help='path to image', default= './datasets/object_images/dataset_29072021/img_1.jpg')
     parser.add_argument('--origin', nargs="+", help='coordinates of origin point by(x,y)', default=['583','30'])
     parser.add_argument('--out_dir', type=str, default="./out_dir")
     args = parser.parse_args()
     return args
 
 def run_pipeline(args):
-    detector = ObjectDetector(args)
+    detector = ObjectDetector()
     calibrator = Calibrator(args)
     regressor = Regressor(args)
     image = cv2.imread(args.image)
     objects = detector.predict(image)
     object_locations = []
     for o in objects:
-        calib_loc = calibrator.predict(o.center)
+        calib_loc = calibrator.predict(o['center'])
         reg_loc = regressor.predict(calib_loc)
-        object_locations.append([o.class_name, reg_loc])
+        object_locations.append([o['class_name'], reg_loc])
     return object_locations
 def main():
     args = parse_args()
